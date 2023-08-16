a7e7d196e78c10fce1a355d014bd457c5e9735fc
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-16 20:22:18 -0300

    Entities & SecurityConfig

--- src/main/java/com/monitasker/entity/Task.java
+++ src/main/java/com/monitasker/entity/Task.java
@@ -1,13 +1,19 @@
 package com.monitasker.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "task")
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_tasks")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,5 +23,13 @@ public class Task {
     @ManyToOne(cascade = CascadeType.ALL, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Column(name = "is_done", nullable = false)
+    @JdbcTypeCode(SqlTypes.BOOLEAN)
+    private Boolean isDone = false;
+
+    @Column(name = "content")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private String content;
 
 }
--- src/main/java/com/monitasker/entity/User.java
+++ src/main/java/com/monitasker/entity/User.java
@@ -1,23 +1,86 @@
 package com.monitasker.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.type.SqlTypes;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
+@Builder
 @Entity
-@Table(name = "user")
+@Table(name = "TB_users")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(nullable = false, unique = true)
     private Long id;
 
+    @Column(length = 50, nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.NVARCHAR)
+    @Email
+    private String email;
+
+    @Lob
+    @Column(name = "passwordhash", nullable = false)
+    @JdbcTypeCode(SqlTypes.BINARY)
+    private byte[] passwordHash;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
     private List<Task> tasks = new ArrayList<>();
 
+    // PASSWORD STORING
+
+    @Transient
+    private String password;
+
+    @PostLoad
+    public void onLoad() {
+        this.password = new String(passwordHash, StandardCharsets.UTF_8);
+    }
+
+    @PrePersist
+    @PreUpdate
+    public void onSave() {
+        if (password != null) {
+            this.passwordHash = password.getBytes(StandardCharsets.UTF_8);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
