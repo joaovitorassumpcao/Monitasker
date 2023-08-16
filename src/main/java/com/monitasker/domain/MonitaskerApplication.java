d47add8c501ea1fba90a27be4d9dd272a52d61b0
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-15 21:03:24 -0300

    Initial

---
+++ src/main/java/com/monitasker/domain/MonitaskerApplication.java
@@ -0,0 +1,13 @@
+package com.monitasker.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitaskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitaskerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monitasker/domain/MonitaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitasker.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
