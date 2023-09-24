c7714838fd0410479bddf4a9f3fe6c3c492a301d
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-09-24 19:22:48 -0300

    Views + Security

--- src/main/java/com/monitasker/model/dto/UserDTO.java
+++ src/main/java/com/monitasker/model/dto/UserDTO.java
@@ -1,7 +1,7 @@
 package com.monitasker.model.dto;
 
 import com.monitasker.model.entity.Task;
-import com.monitasker.model.enums.UserRole;
+import com.monitasker.model.enums.Role;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
@@ -12,7 +12,7 @@ import java.util.List;
 
 public record UserDTO(Long id, String username, @Email String email,
                       byte[] passwordHash, List<Task> tasks,
-                      @Enumerated(EnumType.STRING) UserRole userRole) implements Serializable {
+                      @Enumerated(EnumType.STRING) Role userRole) implements Serializable {
     @Serial
     private final static long serialVersionUID = 1L;
 }
---
+++ src/main/java/com/monitasker/model/enums/Role.java
@@ -0,0 +1,5 @@
+package com.monitasker.model.enums;
+
+public enum Role {
+    USER, ADMIN;
+}
